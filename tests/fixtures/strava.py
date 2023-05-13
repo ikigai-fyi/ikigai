9904b3baac2379060383bc0561c66ccdfb23d574
Paul Nicolet <nicoletpaul@orange.fr>
2023-05-13 10:47:39 +0200

    Add real activity data

--- tests/fixtures/strava.py
+++ tests/fixtures/strava.py
@@ -1,7 +1,10 @@
 import pytest
 import responses
 
-from .resources.activity import ACTIVITY
+from .resources.bike import BIKE_WITH_PICTURES_PREVIEW
+from .resources.climb import CLIMB_NO_PICTURE_PREVIEW
+from .resources.hike import HIKE_WITH_PICTURES_PREVIEW
+from .resources.run import RUN_WITH_PICTURES_PREVIEW
 
 STRAVA_URL = "https://www.strava.com/api/v3"
 
@@ -11,6 +14,11 @@ def get_activities_response_mock(requests_mock):
     requests_mock.add(
         responses.GET,
         f"{STRAVA_URL}/athlete/activities",
-        json=[ACTIVITY],
+        json=[
+            RUN_WITH_PICTURES_PREVIEW,
+            BIKE_WITH_PICTURES_PREVIEW,
+            CLIMB_NO_PICTURE_PREVIEW,
+            HIKE_WITH_PICTURES_PREVIEW,
+        ],
     )
     yield requests_mock
