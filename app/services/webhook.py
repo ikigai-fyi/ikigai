1792a99569b017088190e8db303e9e1cc8a92e78
Paul Nicolet <nicoletpaul@orange.fr>
2023-06-11 17:49:04 +0200

    Log webhooks

--- app/services/webhook.py
+++ app/services/webhook.py
@@ -1,9 +1,16 @@
-from app.schemas.inputs.webhook import StravaWebhookInput
+from flask import current_app
+
 from app.models.athlete import Athlete
+from app.schemas.inputs.webhook import StravaWebhookInput
+
 from .activity import fetch_and_store_activity
 
 
 def handle_strava_webhook(input: StravaWebhookInput):
+    current_app.logger.info(
+        f"Received Strava webhook: {input.object_type.value}:{input.aspect_type.value}"
+    )
+
     if input.is_create_activity or input.is_update_activity:
         athlete = Athlete.get_by_strava_id_or_404(input.owner_id)
         fetch_and_store_activity(input.object_id, athlete)
