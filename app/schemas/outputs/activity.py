7554e9eac45b8260b5f7ca46c40a94f6f7e1855b
Paul Nicolet <nicoletpaul@orange.fr>
2023-08-12 16:28:42 +0200

    Add N rules

--- app/schemas/outputs/activity.py
+++ app/schemas/outputs/activity.py
@@ -23,6 +23,6 @@ class ActivityOutput(BaseModel):
     picture_urls: list[str] | None
 
     @root_validator
-    def fill_picture_urls(cls, values) -> dict:
+    def fill_picture_urls(self, values) -> dict:
         values["picture_urls"] = [values["picture_url"]]
         return values
