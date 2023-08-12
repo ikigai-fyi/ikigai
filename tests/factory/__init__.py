7de29c7615e352087378818bdda8bf7f80c17710
Paul Nicolet <nicoletpaul@orange.fr>
2023-08-12 15:26:26 +0200

    Fix mypy in tests

--- tests/factory/__init__.py
+++ tests/factory/__init__.py
@@ -2,4 +2,6 @@ from sqlalchemy.orm import scoped_session
 
 from app import db
 
-session_factory = scoped_session(lambda: db.session, scopefunc=lambda: db.session)
+session_factory: scoped_session = scoped_session(
+    session_factory=lambda: db.session, scopefunc=lambda: db.session  # type: ignore
+)
