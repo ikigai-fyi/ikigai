28fc0dc8a36e845b1b8418b90a9872ecd3193695
Paul Nicolet <nicoletpaul@orange.fr>
2023-11-08 08:27:00 +0100

    Create fetch jobs upon login

--- app/services/task.py
+++ app/services/task.py
@@ -31,24 +31,24 @@ def with_app_context():
 
 @task
 @with_app_context()
-def fetch_and_store_activities_async(athlete_id: int):
+def create_activities_fetch_jobs_async(athlete_id: int):
     athlete = Athlete.get_by_id(athlete_id)
     client = get_strava_client(athlete)
     activities_iterator = client.get_activities()
 
-    job = None
-    for activity in activities_iterator:
-        is_not_stored = Activity.get_by_strava_id(activity.id) is None
-        if is_not_stored and activity.total_photo_count:
-            job = ActivityFetchJob(
-                athlete_id=athlete.id,
-                activity_strava_id=activity.id,
-            )
-            job.add(commit=True)
+    jobs = [
+        ActivityFetchJob(
+            athlete_id=athlete.id,
+            activity_strava_id=activity.id,
+        ).add(commit=True)
+        for activity in activities_iterator
+        if _should_create_job(activity)
+    ]
 
-    # Launch the first job
-    job_id = job.id if job else None
-    process_activity_fetch_job_async(job_id)
+    # Launch 10 random jobs now to preserve rate limiter
+    jobs_to_process_now = random.sample(jobs, min(10, len(jobs)))
+    for job in jobs_to_process_now:
+        process_activity_fetch_job_async(job_id=job.id)
 
 
 @task
@@ -60,19 +60,21 @@ def process_activity_fetch_job_async(job_id: int | None = None):
     else:
         job = ActivityFetchJob.get_job_to_process()
 
-    if (not job) or (job and job.is_done):
-        # What's next?
-        if not ActivityFetchJob.is_queue_empty():
-            process_activity_fetch_job_async()
-
+    if not job:
         return
 
     athlete = Athlete.get_by_id(job.athlete_id)
     fetch_and_store_activity(job.activity_strava_id, athlete)
     job.mark_as_done()
 
-    # What's next?
-    process_activity_fetch_job_async()
+
+def _should_create_job(activity) -> bool:
+    has_picture = bool(activity.total_photo_count)
+    is_not_downloaded = Activity.get_by_strava_id(activity.id) is None
+    is_not_pending_download = (
+        ActivityFetchJob.get_pending_by_strava_id(activity.id) is None
+    )
+    return has_picture and is_not_downloaded and is_not_pending_download
 
 
 @task
