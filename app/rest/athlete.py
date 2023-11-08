4dd7aac3428669c79c216f46d4e50d93e2665007
Paul Nicolet <nicoletpaul@orange.fr>
2023-11-08 09:47:01 +0100

    Add endpoint to manually create fetch jobs

---
+++ app/rest/athlete.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from spectree.response import Response
+
+from app.extensions import spectree
+from app.models.athlete import Athlete
+from app.services.task import create_activities_fetch_jobs_async
+
+athlete = Blueprint("athlete", __name__, url_prefix="/athletes")
+
+
+@athlete.post("/<string:uuid>/fetch_activities_async")
+@spectree.validate(resp=Response("HTTP_200"))
+def ep_get_random_activity(uuid: str):
+    athlete = Athlete.get_by_uuid(uuid)
+    create_activities_fetch_jobs_async(athlete.id)
+    return "ok"
