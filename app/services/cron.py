10cfa49221941335005a54fdf957b6b929346531
Paul Nicolet <nicoletpaul@orange.fr>
2023-11-10 16:23:37 +0100

    Add cron monitoring

--- app/services/cron.py
+++ app/services/cron.py
@@ -1,3 +1,5 @@
+from sentry_sdk.crons import monitor
+
 from app.models.activity_fetch_job import ActivityFetchJob
 
 from .task import process_activity_fetch_job_async
@@ -5,6 +7,7 @@ from .task import process_activity_fetch_job_async
 CONSUMPTION_BURST = 5
 
 
+@monitor(monitor_slug="consume-activities-fetch-queue")
 def consume_activities_fetch_queue():
     """We assume this is only called sequentially
     so that the same job cannot be consumed twice in parallel.
